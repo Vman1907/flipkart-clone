58abd7b7b54bcc4968433d36fb7088eb1abbadfa
Adarsh Raj <adarshrj62@gmail.com>
2023-10-22 23:07:07 +0530

    Firebase Error http vs https

--- flipkart-admin/src/firebase/firebase.js
+++ flipkart-admin/src/firebase/firebase.js
@@ -7,12 +7,18 @@ import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
+  
   apiKey: "AIzaSyA_22AUbJZbH3YTuUo8HVyW5zOaE2ofwxo",
   authDomain: "hello-world-7dfd0.firebaseapp.com",
   projectId: "hello-world-7dfd0",
   storageBucket: "hello-world-7dfd0.appspot.com",
   messagingSenderId: "521111554753",
-  appId: "1:521111554753:web:c387f7fb11184a02542240",
+  // apiKey: "AIzaSyDwR21Anl-hk7I5wP0udUKUWhlO8knO6fM",
+  // authDomain: "ecommerce-frontend-16f29.firebaseapp.com",
+  // projectId: "ecommerce-frontend-16f29",
+  // storageBucket: "ecommerce-frontend-16f29.appspot.com",
+  // messagingSenderId: "75599244091",
+  // appId: "1:75599244091:web:10adc6629042ba7f13a51e"
 };
 
 const app=firebase.initializeApp(firebaseConfig);
--- flipkart-admin/src/urlConfig.js
+++ flipkart-admin/src/urlConfig.js
@@ -1,3 +1,3 @@
 // export const api='http://localhost:8000/api'
-export const api='http://ec2-51-20-120-242.eu-north-1.compute.amazonaws.com:8000/api'
+export const api='http://16.16.25.206:8000/api'
 // export const api='http://192.168.239.116:8000/api'
