0584f50a9d146d15366afdeb8247b98619adb7c5
Adarsh Raj <adarshrj62@gmail.com>
2023-10-26 16:10:56 +0530

    Resonsive for mobile devices

--- flipkart-admin/src/urlConfig.js
+++ flipkart-admin/src/urlConfig.js
@@ -1,3 +1,4 @@
 // export const api='http://localhost:8000/api'
-export const api='http://16.16.25.206:8000/api'
+// export const api='http://16.16.25.206:8000/api'
+export const api='https://ecommerce-backend-955b.onrender.com/api'
 // export const api='http://192.168.239.116:8000/api'
